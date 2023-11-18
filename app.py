d26fd1d1d8abd7e0a78fb3849c49a160eca674e6
John Bannon <tecknich@Johns-MacBook-Pro-4.local>
2023-11-18 17:16:29 -0500

    test

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ api_key = 'AIzaSyDGPL1I31RJeAnaDnPoTpbfjNjbp7kvYO0'
 
 def get_place_id(query, api_key):
     url = (f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={query}&inputtype=textquery"
-           f"&fields=type&key={api_key}")
+           f"&fields=types&key={api_key}")
 
     response = requests.get(url)
-    results = response.json().get('results', [])
+    results = response.json().get('candidates', [])
     if results:
-        return results[0].get('type')
+        return results[0].get('types', [])
     else:
         return "No results found"
 
