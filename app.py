b5486a1820ca503e468418450b205c7580cc5549
John Bannon <tecknich@Johns-MacBook-Pro-4.local>
2023-11-18 17:30:04 -0500

    location

--- app.py
+++ app.py
@@ -5,21 +5,33 @@ from transformers import pipeline
 model = pipeline(task="automatic-speech-recognition", model="openai/whisper-large-v3")
 api_key = 'AIzaSyDGPL1I31RJeAnaDnPoTpbfjNjbp7kvYO0'
 
+
 def get_place_id(query, api_key):
     url = (f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={query}&inputtype=textquery"
-           f"&fields=types&key={api_key}")
+           f"&fields=types, geometry&key={api_key}")
 
     response = requests.get(url)
     results = response.json().get('candidates', [])
     if results:
-        return results[0].get('types', [])
+        location = results[0].get('geometry', {}).get('location', {})
+        return results[0].get('types', []), location
     else:
         return "No results found"
 
+
+def search_similar_places(types, location, api_key, radius=5000):
+    # Join types to form a string for the API query
+    types_query = "|".join(types)
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location}&radius={radius}&type={types_query}&key={api_key}"
+    response = requests.get(url)
+    results = response.json().get('results', [])
+    return results
+
+
 st.title("Location Recommender")
 
-prompt = st.chat_input("Input place")
+prompt = st.chat_input("Input place and a location")
+types, location = get_place_id(prompt, api_key)
+results = search_similar_places(types, location, api_key)
 
-type = get_place_id(prompt, api_key)
 st.write(type)
-
