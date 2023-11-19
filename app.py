b065e405daabe264f88efd7cb8cec5fdf98df96c
John Bannon <tecknich@Johns-MacBook-Pro-4.local>
2023-11-18 22:54:24 -0500

    adjustments

--- app.py
+++ app.py
@@ -34,41 +34,44 @@ st.markdown("""
 prompt = st.text_input("Input place and a location")
 
 
+def get_place_id(query, api_key):
+    encoded_query = requests.utils.quote(query)
+    url = (
+        f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={encoded_query}&inputtype=textquery"
+        f"&fields=types,geometry&key={api_key}")
+
+    response = requests.get(url)
+    results = response.json().get('candidates', [])
+    if results:
+        location = results[0].get('geometry', {}).get('location', {})
+        types = results[0].get('types', [])
+        return types, location
+    else:
+        return None, None
+
+
+def search_similar_places(types, location, api_key, radius=5000):
+    location_str = f"{location['lat']},{location['lng']}" if location else ""
+    types_query = "|".join(types)
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location_str}&radius={radius}&type={types_query}&key={api_key}"
+    response = requests.get(url)
+    results = response.json().get('results', [])
+    return results
+
+
+def reverse_geocode(lat, lng, api_key):
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&key={api_key}"
+    response = requests.get(url)
+    results = response.json().get('results', [])
+    if results:
+        # Attempting to extract the full address and city
+        full_address = results[0].get('formatted_address', 'Unknown Address')
+        return full_address
+    else:
+        return "No results found", "Unknown City"
+
+
 def location_recommender(query, api_key):
-    def get_place_id(query, api_key):
-        encoded_query = requests.utils.quote(query)
-        url = (
-            f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={encoded_query}&inputtype=textquery"
-            f"&fields=types,geometry&key={api_key}")
-
-        response = requests.get(url)
-        results = response.json().get('candidates', [])
-        if results:
-            location = results[0].get('geometry', {}).get('location', {})
-            types = results[0].get('types', [])
-            return types, location
-        else:
-            return None, None
-
-    def search_similar_places(types, location, api_key, radius=5000):
-        location_str = f"{location['lat']},{location['lng']}" if location else ""
-        types_query = "|".join(types)
-        url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location_str}&radius={radius}&type={types_query}&key={api_key}"
-        response = requests.get(url)
-        results = response.json().get('results', [])
-        return results
-
-    def reverse_geocode(lat, lng, api_key):
-        url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&key={api_key}"
-        response = requests.get(url)
-        results = response.json().get('results', [])
-        if results:
-            # Attempting to extract the full address and city
-            full_address = results[0].get('formatted_address', 'Unknown Address')
-            return full_address
-        else:
-            return "No results found", "Unknown City"
-
     if prompt:
         types, location = get_place_id(query, api_key)
         if not types:
